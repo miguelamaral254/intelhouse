8909b524fbbf575fc590cfaa319cee5e8a944a2e
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2023-10-25 22:19:30 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,21 @@ import Nav from "./components/Nav";
 import About from "./components/About";
 import Benefits from "./components/Benefits";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div
       className="
-    bg-site 
-    bg-no-repeat 
-    bg-cover 
-    overflow-hidden
+  
     "
     >
       <Nav />
       <Banner />
       <About />
       <Benefits />
-      
       <Contact />
+      <Footer />
     </div>
   );
 };
